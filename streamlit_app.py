d065e471fd5f9a85589e0901524790d8cd998b77
Sandeep Singh <113373449+sans2k7@users.noreply.github.com>
2022-09-12 22:00:02 +0530

    Update streamlit_app.py
    Draft 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('This is draft copy')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
