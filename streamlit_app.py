4e9ff9155e52234fc0379ce160e8cdd5971f94c1
Sandeep Singh <113373449+sans2k7@users.noreply.github.com>
2022-09-19 16:49:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,8 @@ streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
 #my_data_row.append(add_my_fruit)
-df1=streamlit.dataframe(my_data_row)
-df2=df1.append(add_my_fruit)
-df2
+#df1=streamlit.dataframe(my_data_row)
+my_data_row.append(add_my_fruit)
+#df2=df1.append(add_my_fruit)
+#df2
+streamlit.dataframe(my_data_row)
