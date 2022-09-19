95da5d096bb42251eb1cf22b0acf458df820423d
Sandeep Singh <113373449+sans2k7@users.noreply.github.com>
2022-09-19 13:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
